71e45718e4946932e4bacf486e769cd07a65552d
singhDigvij <146316192+singhDigvij@users.noreply.github.com>
2023-12-06 05:26:43 +0000

    name change

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import readlineSync from "readline-sync"
 // taking input from user through name
 const userName = readlineSync.question("What is your name ?")
-console.log("Welcome", userName.toUpperCase(),"😀", "Test your IQ here😎" )
+console.log("Welcome", userName.toUpperCase(), "😀", "Test your IQ here😎")
 let score = 0
 // procesing part via function
 
-function play(question, answer){
+function play(question, answer) {
   const userAnswer = readlineSync.question(question)
 
-  if(userAnswer === answer){
+  if (userAnswer === answer) {
     console.log("You are right✅")
-    score = score +1
+    score = score + 1
     console.log("Your current score is:", score)
-  } else{
+  } else {
     console.log("You are wrong❌")
-    score= score - 1
+    score = score - 1
     console.log("Your current score is:", score)
 
   }
@@ -22,38 +22,38 @@ function play(question, answer){
 }
 
 const questions = [{
-  question:"Did chandrayaan-3 made a successful landing on lunar south pole ?",
-  answer:"yes"
-},{
-  question:"What is the name of indigenously built aircraft carrier for Indian Navy ?",
-  answer:"vikrant"
-},{
-  question:"What is the name of indigenously built fighter aircraft for Indian Air Force ?",
-  answer:"tejas"
-},{
-  question:"In which country the biggest cricket stadium of the world  located ?",
-  answer:"india"
-},{
-  question:"Name of India's first homemade covid vaccine ?",
-  answer:"covaxin"
-},{
-  question:"Total number of lok sabha seats 545 or 546 ?",
-  answer:"545"
-},{
-  question:"National tree of India banyan or peepal ?",
-  answer:"banyan"
-},{
-  question:"100% Organic state of the world Assam or Sikkim ? ",
-  answer:"sikkim"
-},{
-  question:"Name of India's oldest city ?",
-  answer:"varanasi"
-},{
-  question:"River ganga originates from which state ?",
-  answer:"uttarakhand"
+  question: "Did chandrayaan-3 made a successful landing on lunar south pole ?",
+  answer: "yes"
+}, {
+  question: "What is the name of indigenously built aircraft carrier for Indian Navy ?",
+  answer: "vikrant"
+}, {
+  question: "What is the name of indigenously built fighter aircraft for Indian Air Force ?",
+  answer: "tejas"
+}, {
+  question: "In which country the biggest cricket stadium of the world  located ?",
+  answer: "india"
+}, {
+  question: "Name of India's first homemade covid vaccine ?",
+  answer: "covaxin"
+}, {
+  question: "Total number of lok sabha seats 545 or 546 ?",
+  answer: "545"
+}, {
+  question: "National tree of India banyan or peepal ?",
+  answer: "banyan"
+}, {
+  question: "100% Organic state of the world Assam or Sikkim ? ",
+  answer: "sikkim"
+}, {
+  question: "Name of India's oldest city ?",
+  answer: "varanasi"
+}, {
+  question: "River ganga originates from which state ?",
+  answer: "uttarakhand"
 },]
 
-for(let i=0; i<questions.length; i++){
+for (let i = 0; i < questions.length; i++) {
   play(questions[i].question, questions[i].answer)
 }
 
@@ -62,20 +62,20 @@ console.log("Your Final Score is:", score)
 
 console.log("High Scorers🏅")
 
-const highScorers =[
+const highScorers = [
   {
-    name:"Aman👨",
-    score:9
+    name: "Aman👨",
+    score: 9
 
   },
   {
-    name:"Janvi👩",
-      score:8
+    name: "Jessi👩",
+    score: 8
   }
 
 ]
 
-for(let i = 0; i<highScorers.length; i++){
+for (let i = 0; i < highScorers.length; i++) {
   console.log(highScorers[i].name),
     console.log("Score:", highScorers[i].score),
     console.log("<---->")
